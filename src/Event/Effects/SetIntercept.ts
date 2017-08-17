8ea3bf7699fb3167c5d104aa84e9ed1e1a4c9cfe
Kyle Boutette <kyleveb@gmail.com>
2017-08-17 05:59:57 +0000

    SetIntercept introduce optional Expiry semantics
    This will allow SetIntercept to completely control its lifecycle
    when registered. The dependency on knowing the identity of an
    interceptor to target it with RemoveIntercept is removed by
    only allow SetIntercept's expiry Mutator to Affix in a
    RemoveIntercept Effect.
    
    InterceptThrowGuardAndRemove relies on the Expiry semantics
    established by this change. Previously, I thought there was an
    easy solution to get around the identity and keep our declarativity
    but tracing out the dependency has made it clear this is needed.

--- src/Event/Effects/SetIntercept.ts
+++ src/Event/Effects/SetIntercept.ts
@@ -15,16 +15,77 @@ import { IAsInterceptor } from '../../Entity/Entities/AsInterceptor';
 import {
     NewEntityCode,
 } from '../../Entity/EntityCode';
+import RemoveIntercept,
+    { IRemoveInterceptorEffectPack } from './RemoveIntercept';
+import Affix, { IAffixMutator, AffixMutatorPlacment } from '../Mutators/Affix';
 
 export interface ISetInterceptorEffectPack extends IEffectPack {
     Filter: IEffectPackFilter;
     Mutator: IEffectPackMutator;
+
+    /**
+     * Expiry being set will establish an additional intercept
+     * which will trigger on the filtered Effect to remove
+     * this interceptor.
+     *
+     * Expiry will be removed after it fires once.
+     */
+    Expiry?: IEffectPackFilter;
 }
 export function AsInterceptor(e: IEffectPack): ISetInterceptorEffectPack {
     if (e.Effect !== Self) {
         throw EffectPackAssertFail(Self, e.Effect);
     }
     return e as ISetInterceptorEffectPack;
+}
+
+/**
+ * registerExpiry sets an interceptor to remove the interceptor
+ * identified by the provided identity when the expiryFilter matches
+ * an Effect.
+ *
+ * Note: the expiry will expire both the identified expiry as well as
+ *       itself, that means it cleans up after it has executed.
+ * @param state where the interceptor will be registered
+ * @param expiryFilter filter which must match to expire the interceptor
+ * @param identity identifier for the interceptor which will expire
+ */
+function registerExpiry(state: IGameState,
+    expiryFilter: IEffectPackFilter, identity: EntityCode): IGameState {
+
+    let expiryIdentity: EntityCode = '';
+    getRNGContext(state, (rng) => {
+        expiryIdentity = NewEntityCode(rng);
+    });
+
+    let expiry = {
+        Source: identity,
+        Targets: [identity],
+        TargetType: TargetType.Interceptor,
+        Effect: RemoveIntercept.Self,
+    } as IRemoveInterceptorEffectPack;
+    let selfExpiry = {
+        Source: expiryIdentity,
+        Targets: [expiryIdentity],
+        TargetType: TargetType.Interceptor,
+        Effect: RemoveIntercept.Self,
+    } as IRemoveInterceptorEffectPack;
+    let expiryInterceptor: IAsInterceptor = {
+        Identity: expiryIdentity,
+
+        IsInterceptor: true,
+        Filter: expiryFilter,
+        Mutator: {
+            Mutator: Affix.Self,
+            Others: [expiry, selfExpiry],
+            // Placement after is very important to allow them to fire
+            // if they would on the event that should remove them.
+            Placement: AffixMutatorPlacment.After,
+        } as IAffixMutator,
+    };
+    state.interceptors.push(expiryInterceptor);
+
+    return state;
 }
 
 /**
@@ -60,6 +121,10 @@ export function Op(state: IGameState, pack: IEffectPack) {
 
     state.interceptors.push(interceptor);
 
+    // Register expiry if declared
+    if (interceptorPack.Expiry) {
+        state = registerExpiry(state, interceptorPack.Expiry, identity);
+    }
     return state;
 }
 
