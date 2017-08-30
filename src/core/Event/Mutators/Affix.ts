523e65e3bc435b4a9d82d8efea5f8c734a10c54e
Kyle Boutette <kyleveb@gmail.com>
2017-08-30 23:20:56 +0000

    core Affix mutator allow dynamic property injection
    Previously, you could create only static Affixes -- with constant
    properties. While these allowed us to express entity-specific
    effects, they failed to allow entity-generic effects.
    
    This new affix gives the ability to copy over properties of the
    triggering EffectPack to the affixed in EffectPack.
    
    This only allows copying of properties rather than arbitary
    injection to prevent Affix from trivially stepping around the type
    sysytem.
    
    Consider the case of Monopoly's 'passing go' as an interceptor.
    Affix can now express the mutator portion by specifying a
    Pay Effect where the replacement is on the Target of the triggering
    Move Effect. Hence, we can now enforce at least some flavors of
    game invariants using interceptors.

--- src/core/Event/Mutators/Affix.ts
+++ src/core/Event/Mutators/Affix.ts
@@ -3,6 +3,14 @@ import {
     EffectPackMutatorAssertFail,
 } from '../Header';
 
+/**
+ * IAffixEffectPack extends IEffectPack to allow property replacements
+ * to be described using a single field.
+ */
+export interface IAffixReplaceEffectPack extends IEffectPack {
+    Replacements?: Array<string>;
+}
+
 export enum AffixMutatorPlacment {
     Before = 'before-initial',
     After = 'after-initial',
@@ -10,7 +18,7 @@ export enum AffixMutatorPlacment {
 export interface IAffixMutator extends IEffectPackMutator {
     Placement: AffixMutatorPlacment;
 
-    Others: Array<IEffectPack>;
+    Others: Array<IAffixReplaceEffectPack>;
 }
 export function AsAffix(e: IEffectPackMutator): IAffixMutator {
     if (e.Mutator !== Self) {
@@ -20,18 +28,50 @@ export function AsAffix(e: IEffectPackMutator): IAffixMutator {
 }
 
 /**
+ * AffixOthersAsMutable returns a copy of the provided effect packs
+ * which can be mutated independently of the source.
+ */
+function AffixOthersAsMutable(
+    original: Array<IAffixReplaceEffectPack>,
+): Array<IAffixReplaceEffectPack> {
+
+    return JSON.parse(JSON.stringify(original));
+}
+
+/**
+ * PerformReplacements replaces properties on mutClone with the
+ * specified properties of source.
+ */
+function PerformReplacements(
+    source: IEffectPack, mutClone: IAffixReplaceEffectPack,
+): IAffixReplaceEffectPack {
+
+    if (mutClone.Replacements === undefined) return mutClone;
+
+    mutClone.Replacements.forEach(replace => {
+        mutClone[replace] = source[replace];
+    });
+
+    return mutClone;
+}
+
+/**
  * Affix adds the 'Others' effects to be executed either
  * before or after. It does not modify the initial Effect.
  */
 export function Op(pack: IEffectPack, mutator: IEffectPackMutator) {
     let affix = AsAffix(mutator);
+
+    let cloned = AffixOthersAsMutable(affix.Others)
+        .map(a => PerformReplacements(pack, a));
+
     let included: Array<IEffectPack>;
     switch (affix.Placement) {
         case AffixMutatorPlacment.Before:
-            included = [...affix.Others, pack];
+            included = [...cloned, pack];
             break;
         case AffixMutatorPlacment.After:
-            included = [pack, ...affix.Others];
+            included = [pack, ...cloned];
             break;
         default:
             throw Error('fell through exhaustive affix.Placement switch');
